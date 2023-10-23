5a2d2883976b16f01da1875c2762520cc813763a
Maciej Stojak <maciej.stojak89@wp.pl>
2023-10-23 10:50:51 +0200

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from './services/modal.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clips';
+
 }
