56423bd9a50f3dbacf586e838840ae6150c58938
Maciej Stojak <maciej.stojak89@wp.pl>
2023-10-24 14:49:43 +0200

    login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+credentials = {
+  email: '',
+  password: '',
+}
+login() {
+  console.log('credentials :>> ', this.credentials);
+}
 }
